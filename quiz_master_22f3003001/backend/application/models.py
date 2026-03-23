ac3fec40cc82d0fbb2af44a534cc05034b38f5da
Disha13raghu <disha13raghu@gmail.com>
2026-03-23 17:15:18 +0530

    Update models.py

--- quiz_master_22f3003001/backend/application/models.py
+++ quiz_master_22f3003001/backend/application/models.py
@@ -38,12 +38,7 @@ class Subject(db.Model):
     chapters = db.relationship('Chapter', backref='subject', lazy=True, cascade="all, delete")
     
     
-class Chapter(db.Model): 
-    id= db.Column(db.Integer,primary_key=True)   
-    subject_id=db.Column(db.Integer,db.ForeignKey("subject.id"),nullable=False)
-    name=db.Column(db.String,unique=True,nullable=False)
-    description=db.Column(db.String)
-    quizes = db.relationship('Quiz', backref='chapter', lazy=True, cascade="all, delete") 
+
     
 
 class Quiz(db.Model):
